8d870c325c11d9032aa0158384066502e083845b
Anıl Şenay <anilsenay3@gmail.com>
2020-10-28 21:33:44 +0300

    Add navigation links to change list type

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./nav.module.css";
 
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <nav className={styles.navigation}>
         <h2 className={styles.logo}>logo</h2>
-        <a href="/[id]/artists" className={styles.navItem}>
+        <a
+          href={router.query.id && "/" + router.query.id + "/artists"}
+          as="[id]/[type]"
+          className={styles.navItem}
+        >
           Artists
         </a>
-        <a href="/[id]/tracks" className={styles.navItem}>
+        <a
+          href={router.query.id && "/" + router.query.id + "/tracks"}
+          as="[id]/[type]"
+          className={styles.navItem}
+        >
           Tracks
         </a>
         <a href="/" className={styles.login}>

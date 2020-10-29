3ab04ad29e9047a322e0c651a62b3d331efda8e2
Anıl Şenay <anilsenay3@gmail.com>
2020-10-29 17:47:50 +0300

    Create new homepage design

---
+++ src/icons/playlist.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function PlaylistIcon(props) {
+  return (
+    <svg viewBox="0 0 405.333 405.333" {...props}>
+      <path d="M0 53.333h256V96H0zM0 138.667h256v42.667H0z" />
+      <path d="M298.667 53.333v174.613c-6.72-2.453-13.76-3.947-21.333-3.947-35.307 0-64 28.693-64 64s28.693 64 64 64 64-28.693 64-64V96h64V53.333H298.667zM0 224h170.667v42.667H0z" />
+    </svg>
+  );
+}
+
+export default PlaylistIcon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
+import Image from "next/image";
+
+import PlaylistIcon from "../icons/playlist";
+import ClockIcon from "../icons/clock";
+import ShareIcon from "../icons/share";
 
 export default function Home() {
   const router = useRouter();
@@ -15,9 +20,44 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <a href="https://accounts.spotify.com/authorize?client_id=c96d35f8f1314c7a8d2b4694992e39ee&response_type=token&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin&scope=user-read-private%20user-read-email%20user-top-read%20playlist-modify-public%20playlist-modify-private%20user-read-private%20user-read-recently-played&state=34fFs29kd09&show_dialog=true">
+        {/* <a href="https://accounts.spotify.com/authorize?client_id=c96d35f8f1314c7a8d2b4694992e39ee&response_type=token&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin&scope=user-read-private%20user-read-email%20user-top-read%20playlist-modify-public%20playlist-modify-private%20user-read-private%20user-read-recently-played&state=34fFs29kd09&show_dialog=true">
           Button
-        </a>
+        </a> */}
+        <div className={styles.mainContent}>
+          <div className={styles.contentTexts}>
+            <h1>Your stats for Spotify</h1>
+            <h3>
+              Login with your Spotify account to see and share your stats!
+            </h3>
+            <a
+              className={styles.button}
+              href="https://accounts.spotify.com/authorize?client_id=c96d35f8f1314c7a8d2b4694992e39ee&response_type=token&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin&scope=user-read-private%20user-read-email%20user-top-read%20playlist-modify-public%20playlist-modify-private%20user-read-private%20user-read-recently-played&state=34fFs29kd09&show_dialog=true"
+            >
+              Login with Spotify
+            </a>
+            <span className={styles.privacyText}>
+              By logging in, you agree to our privacy policy
+            </span>
+            <div className={styles.features}>
+              <div>
+                <PlaylistIcon width={110} height={110} />
+                <h4>Your Top 50 Lists</h4>
+                <span>See your most listened top 50 Artists and Tracks.</span>
+              </div>
+              <div>
+                <ClockIcon width={100} height={100} />
+                <h4>3 Differnt Time Periods</h4>
+                <span>See your top 50 lists in different 3 time periods</span>
+              </div>
+              <div>
+                <ShareIcon width={100} height={100} />
+                <h4>Share Your Lists</h4>
+                <span>Share your lists for others by copy your unique URL</span>
+              </div>
+            </div>
+          </div>
+          <Image src="/home-bg.png" width="538" height="565" />
+        </div>
       </main>
     </div>
   );

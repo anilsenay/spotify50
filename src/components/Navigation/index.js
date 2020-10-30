a963a8ded0b251caaefd086d8853d3b7d6adf60d
Anıl Şenay <anilsenay3@gmail.com>
2020-10-30 16:51:32 +0300

    Update nav links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,9 +3,15 @@ import styles from "./nav.module.css";
 
 import { useRouter } from "next/router";
 import Link from "next/link";
+import appHook from "../../hooks/app.hook";
 
 export default function Navigation() {
   const router = useRouter();
+  const { useAppState } = appHook();
+  const { profile, list_id } = useAppState();
+
+  const loginUrl =
+    "https://accounts.spotify.com/authorize?client_id=c96d35f8f1314c7a8d2b4694992e39ee&response_type=token&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin&scope=user-read-private%20user-read-email%20user-top-read%20playlist-modify-public%20playlist-modify-private%20user-read-private%20user-read-recently-played&state=34fFs29kd09&show_dialog=true";
 
   return (
     <div className={styles.container}>
@@ -14,25 +20,45 @@ export default function Navigation() {
         <Link href="/">
           <img src="/logo.png" className={styles.logo} />
         </Link>
-        <a
-          href={router.query.id && "/" + router.query.id + "?type=artists"}
-          as="[id]/[type]"
-          className={styles.navItem}
-          style={{ color: router.query?.type !== "tracks" && "#1db954" }}
+        <Link
+          href={
+            router.query.id || list_id
+              ? "/" + (router.query.id || list_id) + "?type=artists"
+              : loginUrl
+          }
         >
-          Artists
-        </a>
-        <a
-          href={router.query.id && "/" + router.query.id + "?type=tracks"}
-          as="[id]/[type]"
-          className={styles.navItem}
-          style={{ color: router.query?.type === "tracks" && "#1db954" }}
+          <a
+            as="[id]/[type]"
+            className={styles.navItem}
+            style={{ color: router.query?.type !== "tracks" && "#1db954" }}
+          >
+            Artists
+          </a>
+        </Link>
+        <Link
+          href={
+            router.query.id || list_id
+              ? "/" + (router.query.id || list_id) + "?type=tracks"
+              : loginUrl
+          }
         >
-          Tracks
-        </a>
-        <a href="/" className={styles.login}>
-          Login
-        </a>
+          <a
+            as="[id]/[type]"
+            className={styles.navItem}
+            style={{ color: router.query?.type === "tracks" && "#1db954" }}
+          >
+            Tracks
+          </a>
+        </Link>
+        {list_id && profile ? (
+          <a href="/account" className={styles.login}>
+            My Account
+          </a>
+        ) : (
+          <a href={loginUrl} className={styles.login}>
+            Login
+          </a>
+        )}
       </nav>
     </div>
   );
